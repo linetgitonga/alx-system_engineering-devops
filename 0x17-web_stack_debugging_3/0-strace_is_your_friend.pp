f188941712fed88ea10347c0b12efb4b30acee54
linetgitonga <133949141+linetgitonga@users.noreply.github.com>
2024-05-28 04:12:58 +0300

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# fixes the Apache 500 error by reconstructing typo in wordpress
-exec { 'fix typo':
-  onlyif  => 'test -e /var/www/html/wp-settings.php',
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
--- 0x17-web_stack_debugging_3/README.md
+++
@@ -1,2 +0,0 @@
-## 0x17-web_stack_debugging_3
-fixing the 500 error in 
