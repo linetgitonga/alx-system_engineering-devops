f5c43629642f7b2e06788fc2616cc1af3fa43fb1
linetgitonga <linetgitonga55@gmail.com>
2024-06-13 11:17:48 +0000

    debugging

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+#Should enable the user holberton to login and open files without error.
+
+# Increasing  hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increasing the soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,3 @@
+## web_stack_debugging_4
+#0-the_sky_is_the_limit_not.pp
+#: 1-user_limit.pp
